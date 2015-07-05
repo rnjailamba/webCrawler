5c46732aab1ecf4f0b7f582ee6aeb35a87579c30
rnjai <rnjailamba12@gmail.com>
2015-07-06 00:07:25 +0530

    Adding the error handling and printing the soup

--- webCrawler.py
+++ webCrawler.py
@@ -1,14 +1,59 @@
+import urllib2, httplib
 from bs4 import BeautifulSoup
-
 import requests
+import sys
 
-url = raw_input("Enter a website to extract the URL's from: ")
+def append_log(message):
+    print message
 
-r  = requests.get(url)
+def get_web_page(address):
+    try:
+        user_agent = 'Mozilla/5.0'
+        headers = { 'User-Agent' : user_agent }
+        request = urllib2.Request(address, None, headers)
+        response = urllib2.urlopen(request, timeout=20)
+        try:
+            return response.read()
+        finally:
+            response.close()
+    except urllib2.HTTPError as e:
+        error_desc = httplib.responses.get(e.code, '')
+        append_log('HTTP Error: ' + str(e.code) + ': ' +
+                  error_desc + ': ' + address)
+    except urllib2.URLError as e:
+        append_log('URL Error: ' + e.reason[1] + address)
+    except Exception as e:
+        append_log('Other Error: ' + str(e) + address)
 
-data = r.text
+def process_web_page(data):
+    if data is not None:
+        soup = BeautifulSoup(data ,"lxml")
+        print(soup)
+    else:
+        get_data()
+        print('do something')
+        pass
+         # do something else
 
-soup = BeautifulSoup(data ,"lxml")
+def request_data():
+    url = raw_input("Enter a website to extract the URL's from/type exit to exit: ")
+    if url == 'exit':
+        sys.exit()   
+    data = get_web_page(url)    
+    process_web_page(data)  
 
-for link in soup.find_all('a'):
-    print(link.get('href'))
+def get_data():
+    url = raw_input("Enter maximum number of Links to be parsed")
+    request_data()
+
+
+get_data()
+
+# data = get_web_page('www.doesnotexistblah.com/')
+# process_web_page(data)
+
+# data = get_web_page('https://github.com/fdxxxxx')
+# process_web_page(data)
+
+#data = get_web_page('http://docs.python.org/copyright.html')
+#process_web_page(data)
